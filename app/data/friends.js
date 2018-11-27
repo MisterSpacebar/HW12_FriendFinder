c826918ba8f8d9b794a879be818778566ba44ef6
Yibin Wei <yibinwei@live.com>
2018-11-27 00:23:44 -0500

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+var friendList = [
+    {
+      name: "Clark Kent",
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    }
+  ]
+  
+  module.exports = friendList;
